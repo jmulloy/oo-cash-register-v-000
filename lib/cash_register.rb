2fdeda3ce75e643e5645ecb5d8e4eb64b679cada
Josh Mulloy <joshmulloy1@gmail.com>
2018-07-03 20:57:53 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,6 +1,6 @@
 class CashRegister
 
-  attr_accessor :cash_register, :cash_register_with_discount
+  attr_accessor :cash_register, :discount
 
   def initialize(total=0, cash_register_with_discount=0)
     @total = 0
